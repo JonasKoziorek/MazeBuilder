64d658c9cc4bba80fd0c0b1f5807c71a1f1e144e
Jonas <jonas.koziorek@gmail.com>
2022-04-08 16:57:14 +0200

    New comments.

--- SVG.h
+++ SVG.h
@@ -32,7 +32,17 @@ class SVG{
         /** image height
          */
         int height;
+        /** Create a line specified by two points.
+         * @param x1 - x coordinate for the first point
+         * @param y1 - y coordinate for the first point
+         * @param x2 - x coordinate for the second point
+         * @param y2 - y coordinate for the second point
+         */
         void CreateLine(int x1, int y1, int x2, int y2);
+        /** Create a line specified by one point and orientation.
+         * @param x1 - x coordinate for the point
+         * @param y1 - y coordinate for the point
+         */
         void CreateLine(int row, int col, bool horizontal);
         SVG(string path, int width, int height);
         /* Finilizes the document where data is saved.
