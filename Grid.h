57aab4937542222f56dc17b2cee7a8dfb19dc38c
Jonas <jonas.koziorek@gmail.com>
2022-04-08 16:27:10 +0200

    Added comments for Grid class

--- Grid.h
+++ Grid.h
@@ -14,13 +14,20 @@ class Grid{
      * @brief Grid class represents grid of rectangular cells.
      */
     public:
+        /**
+         * A constructor for Grid.
+         * @param width - width of a grid
+         * @param height - height of a grid
+         */
         Grid(int width, int height);
         /** 
          * Creates adjacency list (of AdList type) for grid with dims. m * n 
          */
         AdList CreateGraph();
         /** 
-          Creates image of a maze
+         * Creates image of a maze.
+         * @param svg - instance of SVG class which hold information about the final image of the maze.
+         * @param path - i-th element in path corresponds to index of a cell in a grid from which we got to i-th cell
         */
         void CreateMaze(SVG* svg, vector<int> path); 
         /** 
@@ -37,7 +44,7 @@ class Grid{
         unsigned int height; 
         /**
          * Removes connected walls from this->grid.
-         * @param path - i-th entry in path corresponds to Node from which DFS algorithms got to i-th Node
+         * @param path - i-th element in path corresponds to index of a cell in a grid from which we got to i-th cell
          */
         void BreachWalls(vector<int> path);
         /** 
